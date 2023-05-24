5ea3023bd7d84416e426a3f2a863dffc95bb2990
Luigi Pertoldi <luigi.pertoldi@protonmail.com>
2023-05-24 16:12:03 +0200

    Place all tests output files in auto-deleting temporary dir

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,21 +7,18 @@ from tempfile import gettempdir
 import pytest
 from legendtestdata import LegendTestData
 
-_tmptestdir = os.path.join(
-    gettempdir(), "pygama-tests-" + getuser() + str(uuid.uuid4())
-)
+_tmptestdir = os.path.join(gettempdir(), f"lgdo-tests-{getuser()}-{str(uuid.uuid4())}")
 
 
 @pytest.fixture(scope="session")
 def tmptestdir():
     os.mkdir(_tmptestdir)
-    yield _tmptestdir
-    shutil.rmtree(_tmptestdir)
+    return _tmptestdir
 
 
 def pytest_sessionfinish(session, exitstatus):
-    if exitstatus is True:
-        os.rmdir(_tmptestdir)
+    if exitstatus == 0:
+        shutil.rmtree(_tmptestdir)
 
 
 @pytest.fixture(scope="session")
@@ -29,26 +26,3 @@ def lgnd_test_data():
     ldata = LegendTestData()
     ldata.checkout("c089a59")
     return ldata
-
-
-# @pytest.fixture(scope="session")
-# def multich_raw_file(lgnd_test_data):
-#     out_file = "/tmp/L200-comm-20211130-phy-spms.lh5"
-#     out_spec = {
-#         "FCEventDecoder": {
-#             "ch{key}": {
-#                 "key_list": [[0, 6]],
-#                 "out_stream": out_file + ":{name}",
-#                 "out_name": "raw",
-#             }
-#         }
-#     }
-
-#     build_raw(
-#         in_stream=lgnd_test_data.get_path("fcio/L200-comm-20211130-phy-spms.fcio"),
-#         out_spec=out_spec,
-#         overwrite=True,
-#     )
-#     assert os.path.exists(out_file)
-
-#     return out_file
