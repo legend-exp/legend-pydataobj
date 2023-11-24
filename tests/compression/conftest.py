b55ae453a052e55e749ea8a678058668811d7bdf
Neuberger <moritz.neuberger@tum.de>
2023-11-24 11:25:50 +0100

    Moved test files

--- tests/compression/conftest.py
+++ tests/compression/conftest.py
@@ -1,12 +1,12 @@
 import pytest
 
-from lgdo import LH5Store
+import lgdo.lh5 as lh5
 
 
 @pytest.fixture()
 def wftable(lgnd_test_data):
-    store = LH5Store()
-    wft, _ = store.read_object(
+    store = lh5.Store()
+    wft, _ = store.read(
         "/geds/raw/waveform",
         lgnd_test_data.get_path("lh5/LDQTA_r117_20200110T105115Z_cal_geds_raw.lh5"),
     )
