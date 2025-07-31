fix view_as with different units attachment points

src/lgdo/types/encoded.py
@@ -249,13 +249,19 @@ class VectorOfEncodedVectors(LGDOCollection):
         --------
         .LGDO.view_as
         """
-        attach_units = with_units and "units" in self.attrs
+        attach_units = with_units and (
+            "units" in self.attrs or "units" in self.encoded_data.attrs
+        )
 
         if library == "ak":
             records_list = {
                 "encoded_data": self.encoded_data.view_as("ak", with_units=with_units),
                 "decoded_size": np.array(self.decoded_size),
             }
+            if "units" in self.attrs:
+                records_list["encoded_data"] = ak.with_parameter(
+                    records_list["encoded_data"], "units", self.attrs["units"]
+                )
             return ak.Array(records_list)
 
         if library == "np":
@@ -476,7 +482,9 @@ class ArrayOfEncodedEqualSizedArrays(LGDOCollection):
         --------
         .LGDO.view_as
         """
-        attach_units = with_units and "units" in self.attrs
+        attach_units = with_units and (
+            "units" in self.attrs or "units" in self.encoded_data.attrs
+        )
 
         if library == "ak":
             records_list = {
@@ -485,6 +493,10 @@ class ArrayOfEncodedEqualSizedArrays(LGDOCollection):
                     len(self.encoded_data.cumulative_length), self.decoded_size.value
                 ),
             }
+            if "units" in self.attrs:
+                records_list["encoded_data"] = ak.with_parameter(
+                    records_list["encoded_data"], "units", self.attrs["units"]
+                )
             return ak.Array(records_list)
 
         if library == "np":

tests/types/test_vectorofvectors.py
@@ -445,35 +445,37 @@ def test_iter(testvov):
 
 
 def test_view_as(testvov):
-    v2d = testvov.v2d
+    v2ds = [copy.deepcopy(testvov.v2d), copy.deepcopy(testvov.v2d)]
+    v2ds[0].attrs["units"] = "s"
+    v2ds[1].flattened_data.attrs["units"] = "s"
 
-    v2d.flattened_data.attrs["units"] = "s"
-    ak_arr = v2d.view_as("ak", with_units=True)
-    assert isinstance(ak_arr, ak.Array)
-    assert ak.parameters(ak_arr) == {"units": "s"}
+    for v2d in v2ds:
+        ak_arr = v2d.view_as("ak", with_units=True)
+        assert isinstance(ak_arr, ak.Array)
+        assert ak.parameters(ak_arr) == {"units": "s"}
 
-    ak_arr = v2d.view_as("ak", with_units=False)
+        ak_arr = v2d.view_as("ak", with_units=False)
 
-    assert isinstance(ak_arr, ak.Array)
-    assert ak.is_valid(ak_arr)
-    assert len(ak_arr) == len(v2d)
-    assert ak.all(ak_arr == [[1, 2], [3, 4, 5], [2], [4, 8, 9, 7], [5, 3, 1]])
+        assert isinstance(ak_arr, ak.Array)
+        assert ak.is_valid(ak_arr)
+        assert len(ak_arr) == len(v2d)
+        assert ak.all(ak_arr == [[1, 2], [3, 4, 5], [2], [4, 8, 9, 7], [5, 3, 1]])
 
-    np_arr = v2d.view_as("np", with_units=True)
-    assert isinstance(np_arr, pint.Quantity)
-    assert np_arr.u == "second"
-    assert isinstance(np_arr.m, np.ndarray)
+        np_arr = v2d.view_as("np", with_units=True)
+        assert isinstance(np_arr, pint.Quantity)
+        assert np_arr.u == "second"
+        assert isinstance(np_arr.m, np.ndarray)
 
-    np_arr = v2d.view_as("np", with_units=False)
-    assert isinstance(np_arr, np.ndarray)
-    assert np.issubdtype(np_arr.dtype, np.floating)
+        np_arr = v2d.view_as("np", with_units=False)
+        assert isinstance(np_arr, np.ndarray)
+        assert np.issubdtype(np_arr.dtype, np.floating)
 
-    np_arr = v2d.view_as("np", with_units=False, fill_val=0, preserve_dtype=True)
-    assert np.issubdtype(np_arr.dtype, np.integer)
+        np_arr = v2d.view_as("np", with_units=False, fill_val=0, preserve_dtype=True)
+        assert np.issubdtype(np_arr.dtype, np.integer)
 
-    np_arr = v2d.view_as("pd", with_units=False)
-    assert isinstance(np_arr, pd.Series)
-    assert isinstance(np_arr.ak, akpd.accessor.AwkwardAccessor)
+        np_arr = v2d.view_as("pd", with_units=False)
+        assert isinstance(np_arr, pd.Series)
+        assert isinstance(np_arr.ak, akpd.accessor.AwkwardAccessor)
 
     v3d = testvov.v3d
 